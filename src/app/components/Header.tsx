efd5da0da91c3a0f47e10e38172e84118e8a01bb
alexkarumbi <alexkarumbimuhia@gmail.com>
2024-03-21 12:51:33 +0300

    Finished form UI

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex justify-around  mb-7'  >
+        <Image src={'/images/smallLogo.png'} width={200} height={90} alt ={"Ministry Log"} />
+        <h2 className="font-bold text-3xl mt-14" >
+            State Department of culture and heritage ushanga program 
+        </h2>
+        <Image src={'/images/ushangaLogo.png'} width={100} height={90} alt ={"Ministry Log"} />
+    </div>
   )
 }
 
